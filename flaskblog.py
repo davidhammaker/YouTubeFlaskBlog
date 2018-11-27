f10f57494082bb1d3c9d82483db1362bf55bf7b1
David J. Hammaker <davidjosh93@gmail.com>
2018-11-27 10:59:14 -0500

    Add view functions for register and login pages
    Register view creates RegistrationForm instance, and login view creates
    LoginForm instance.

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '6086dbce01e4a17787cf07135787e954'
@@ -28,6 +29,18 @@ def hello():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
