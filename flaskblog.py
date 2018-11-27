6050c5a8b2b9a721c2dcf11a146da2f26ffada2d
David J. Hammaker <davidjosh93@gmail.com>
2018-11-27 10:59:14 -0500

    Include simple home page template
    Using render_template from Flask to render a tiny html file.

--- flaskblog.py
+++ flaskblog.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 @app.route('/home')
 def hello():
-    return "Hello World!"
+    return render_template('home.html')
 
 
 @app.route('/about')
