25f6b3b4c6372cc7a7ad3c71d05ff6bb849dc73e
David J. Hammaker <davidjosh93@gmail.com>
2018-11-27 10:59:14 -0500

    Add dummy posts to home page
    Posts are a list of Python dictionaries inserted with Jinja2.

--- flaskblog.py
+++ flaskblog.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'David Hammaker',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'November 27, 2018'
+    },
+    {
+        'author': 'Aaron Key',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'November 28, 2018'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def hello():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
