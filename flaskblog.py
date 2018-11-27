1cc875f2e9573032cb5dcacec060cca9bc955221
David J. Hammaker <davidjosh93@gmail.com>
2018-11-27 10:59:14 -0500

    Enable (temporary) faux login
    Temporarily, a fake email address and password will simulate logging
    into the site. Successful and failed logins will generate appropriate
    flash messages, and successful logins will redirect to the home page.

--- flaskblog.py
+++ flaskblog.py
@@ -40,9 +40,15 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@example.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check email and password.', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 
