642a9b3fa8a41af43861b4a4563c1d21fab9f5d6
David J. Hammaker <davidjosh93@gmail.com>
2018-11-27 10:59:14 -0500

    Update titles
    Page titles now say 'Flask Blog', with optional additions to title.
    E.g. the About page will include 'About' in the title.

--- flaskblog.py
+++ flaskblog.py
@@ -24,7 +24,7 @@ def hello():
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
