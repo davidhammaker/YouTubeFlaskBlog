c6bbcdcdbd303ba624dbe5ddda8a715aa2e23dea
David J. Hammaker <davidjosh93@gmail.com>
2018-11-29 15:31:37 -0500

    Update flashed message on successful registration
    Message now reads: 'Your account has been created! You are now able to
    log in.'

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -39,6 +39,7 @@ def register():
         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
+        flash('Your account has been created! You are now able to log in.', 'success')
     return render_template('register.html', title='Register', form=form)
 
 
