b4fc3bc9492e53553c3a0ad76ba88d652a3e0bd8
David J. Hammaker <davidjosh93@gmail.com>
2018-11-29 13:17:56 -0500

    Separate the project into a package
    The flaskblog.py file no longer exists. The __init__.py file now
    initiates the Flask app and db session, and imports routes. All models
    are now located in models.py. All view functions are now located in
    routes.py. The app now runs with `$ python run.py`, which contains the
    `if __name__ == '__main__':` conditional.

---
+++ flaskblog/routes.py
@@ -0,0 +1,52 @@
+from flask import render_template, url_for, flash, redirect
+from flaskblog import app
+from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.models import User, Post
+
+
+posts = [
+    {
+        'author': 'David Hammaker',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'November 27, 2018'
+    },
+    {
+        'author': 'Aaron Key',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'November 28, 2018'
+    }
+]
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@example.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check email and password.', 'danger')
+    return render_template('login.html', title='Login', form=form)
