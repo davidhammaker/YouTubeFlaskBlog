a38300d81822b374de86bafc31f9fad10cb75de1
David J. Hammaker <davidjosh93@gmail.com>
2018-11-30 11:39:28 -0500

    Redirect current user to home from register/login
    If the current user navigates to '/register' or '/login', we use
    current_user.is_authenticated (flask-login) to recognize the current
    user and redirect the user to the home page, since the user is already
    registered and logged in.

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
-from flask_login import login_user
+from flask_login import login_user, current_user
 
 
 posts = [
@@ -34,6 +34,8 @@ def about():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data)
@@ -47,6 +49,8 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
