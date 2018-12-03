606be520fefe0408f7a58b9abbbbf08a8adcb23e
David J. Hammaker <davidjosh93@gmail.com>
2018-12-03 12:02:12 -0500

    Create upload form to update profile picture
    Form field is in 'account.html' and will list any errors. Encoding type
    for this form is set to "multipart/form-data" for file upload. Field is
    defined in 'forms.py' as an optional field that only accepts 'jpg' and
    'png' files. Submitting a new image will not yet update the profile
    picture, but the form field itself is operating.

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
@@ -44,6 +45,7 @@ class UpdateAccountForm(FlaskForm):
                                        Length(min=2, max=20)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Update')
 
     def validate_username(self, username):
