c7b44b3886111ef51529b06bba63eb24ee8f61fd
David J. Hammaker <davidjosh93@gmail.com>
2018-12-06 13:43:23 -0500

    Add functionality to 'send_reset_email()' method
    The method creates a token for the user, configures and formats a
    message containing a link (with the token) to the 'reset_token' view,
    and sends the message to the user email.

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,11 +2,12 @@ import os
 import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
-from flaskblog import app, db, bcrypt
+from flaskblog import app, db, bcrypt, mail
 from flaskblog.forms import (RegistrationForm, LoginForm, UpdateAccountForm,
                              PostForm, RequestResetForm, ResetPasswordForm)
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
+from flask_mail import Message
 
 
 @app.route('/')
@@ -153,7 +154,16 @@ def user_posts(username):
 
 
 def send_reset_email(user):
-    pass
+    token = user.get_reset_token()
+    msg = Message('Password Reset Request',
+                  sender='noreply@demo.com',
+                  recipients=[user.email])
+    msg.body = f'''To reset your password, visit the following link:
+{url_for('reset_token', token=token, _external=True)}
+
+If you did not make this request, then simply ignore this email and no changes will be made.
+'''
+    mail.send(msg)
 
 
 @app.route('/reset_password', methods=['GET', 'POST'])
