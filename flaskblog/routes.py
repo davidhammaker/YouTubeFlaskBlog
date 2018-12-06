0703d6c43bfb0d1f270e30bfcba1ef8a82bec41c
David J. Hammaker <davidjosh93@gmail.com>
2018-12-06 10:52:58 -0500

    Add form validation conditional to 'reset_request'
    The 'reset_request' view now contains a conditional that selects the
    user associated with the submitted email address. After selecting the
    user, the user is passed to the 'send_reset_email()' function, which is
    currently a non-functional placeholder. After this, the view sends a
    flash message and redirects to the 'login' page.

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -152,11 +152,20 @@ def user_posts(username):
     return render_template('user_posts.html', posts=posts, user=user)
 
 
+def send_reset_email(user):
+    pass
+
+
 @app.route('/reset_password', methods=['GET', 'POST'])
 def reset_request():
     if current_user.is_authenticated:
         return redirect(url_for('home'))
     form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        send_reset_email(user)
+        flash('An email has been sent with instructions to reset your password.', 'info')
+        return redirect(url_for('login'))
     return render_template('reset_request.html', title='Reset Password', form=form)
 
 
