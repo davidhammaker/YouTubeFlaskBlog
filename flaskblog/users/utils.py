49f286e67f55fe506f5c553008e9d711e36a95fa
David J. Hammaker <davidjosh93@gmail.com>
2018-12-06 18:59:50 -0500

    Make the FlaskBlog app configurable
    App initialization, Config class instance creation, extension
    initialization, and blueprints are all located within a new function in
    '__init__.py', called 'create_app()', which takes a configuration class
    as an argument (Config by default). Instances of 'app' in other files
    have been replaced by 'current_app' from 'flask'. The 'run.py' file now
    imports the 'create_app' function and creates an instance called 'app'
    to run.

--- flaskblog/users/utils.py
+++ flaskblog/users/utils.py
@@ -1,8 +1,8 @@
 import os
 import secrets
 from PIL import Image
-from flask import url_for
-from flaskblog import app, mail
+from flask import url_for, current_app
+from flaskblog import mail
 from flask_mail import Message
 
 
@@ -10,7 +10,7 @@ def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    picture_path = os.path.join(current_app.root_path, 'static/profile_pics', picture_fn)
     output_size = (125, 125)
     i = Image.open(form_picture)
     i.thumbnail(output_size)
