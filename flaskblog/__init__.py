894e5bcbcd824e8a0fb0a5ce817cbca69c1ba653
David J. Hammaker <davidjosh93@gmail.com>
2018-12-06 16:11:14 -0500

    Update 'flaskblog\__init__.py' imports and app
    To account for the use of blueprints, all view blueprints are imported 
    from their individual project folders. Additionally, each set of 
    blueprints is registered to the app.

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -20,4 +20,10 @@ app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
 app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 mail = Mail(app)
 
-from flaskblog import routes
+from flaskblog.users.routes import users
+from flaskblog.posts.routes import posts
+from flaskblog.main.routes import main
+
+app.register_blueprint(users)
+app.register_blueprint(posts)
+app.register_blueprint(main)
